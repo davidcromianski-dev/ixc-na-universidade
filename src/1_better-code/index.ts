4d4090ea3cc21af711bb49b9c46226e0f5b456a3
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2025-09-05 23:31:44 -0300

    Refactor UserController instantiation in main function
    - Updated UserController instantiation to use userService for improved clarity and functionality.
    - Removed commented-out service imports to enhance code readability.

--- src/1_better-code/index.ts
+++ src/1_better-code/index.ts
@@ -8,7 +8,7 @@ async function main() {
   const userService = new UserService();
   // const userRestrictedByDateService = new UserRestrictedByDateService();
   const connection = new Connection({ host: 'localhost', port: 3306 });
-  const controller = new UserController(c, connection);
+  const controller = new UserController(userService, connection);
   
   try {
     // Teste de autenticação
